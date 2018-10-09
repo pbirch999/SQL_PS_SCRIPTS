fb96ede543ab29b23c2ef251d0e23f69968156c1
Patrick Birch <pbirch999@gmail.com>
2018-10-09 16:26:39 -0500

    Add files via upload

--- SEARCH FOR AMS TABLE NAME and COLUMN NAME.sql
+++ SEARCH FOR AMS TABLE NAME and COLUMN NAME.sql
@@ -6,88 +6,99 @@ Populates a result set with the values from a table
 Patrick Birch
 9/26
 */
-DECLARE @prefix2 varchar(2) = 'tb', @prefix3 varchar(3)='stb';
-
-
-
+DECLARE @prefix2 VARCHAR(2) = 'tb',
+	@prefix3 VARCHAR(3) = 'stb';
 -- SET TABLE NAME
-DECLARE @tbName varchar(100) , @colName varchar(200)
+DECLARE @tbName VARCHAR(100),
+	@colName VARCHAR(200)
+
 -- add only the name of the table - the script adds the appropriate prefix
-
-SET @tbName = 'casetopic'   
-
+SET @tbName = 'casetopic'
 -- SET COLUMN NAME
-
- -- Easiest way to limit the results - use "vc" or "dt", or any value, depending on the value to be returned.
-
+-- Easiest way to limit the results - use "vc" or "dt", or any value, depending on the value to be returned.
 SET @colName = 'vc'
 
 --SELECT UPPER(LEFT(@tbName,1))+LOWER(SUBSTRING(@tbName,2,LEN(@tbName)))
 BEGIN
-	If left(@tbname,2) = @prefix2 GOTO META_ONE
-	ELSE  GOTO ADD_PREFIX;
+	IF left(@tbname, 2) = @prefix2
+		GOTO META_ONE
+	ELSE
+		GOTO ADD_PREFIX;
 END
-
 
 ADD_PREFIX:
 
-If NOT EXISTS (SELECT TABLE_NAME FROM information_schema.tables WHERE TABLE_NAME = @tbName) 
+IF NOT EXISTS (
+		SELECT TABLE_NAME
+		FROM information_schema.tables
+		WHERE TABLE_NAME = @tbName
+		)
+BEGIN
+	SET @tbName = UPPER(LEFT(@tbName, 1)) + LOWER(SUBSTRING(@tbName, 2, LEN(@tbName)))
+	SET @tbName = @prefix3 + @tbName
 
-	BEGIN
-		SET @tbName = UPPER(LEFT(@tbName,1))+LOWER(SUBSTRING(@tbName,2,LEN(@tbName)))
-		SET @tbName = @prefix3 + @tbName
-		SELECT @tbname as STB_STEP
-
-	END
+	SELECT @tbname AS STB_STEP
+END
 ELSE
-	BEGIN
-	
-		SET @tbName = @prefix2 + SUBSTRING(@tbName,4,100)
-		SET @tbName =  LOWER( LEFT(UPPER(LEFT(@tbName, 3)),2)) + UPPER(SUBSTRING(@tbName,3,LEN(1))) + LOWER(SUBSTRING(@tbName,4,LEN(@tbName)))
-		SELECT @tbname AS TB_STEP
-	END
+BEGIN
+	SET @tbName = @prefix2 + SUBSTRING(@tbName, 4, 100)
+	SET @tbName = LOWER(LEFT(UPPER(LEFT(@tbName, 3)), 2)) + UPPER(SUBSTRING(@tbName, 3, LEN(1))) + LOWER(SUBSTRING(@tbName, 4, LEN(@tbName)))
 
+	SELECT @tbname AS TB_STEP
+END
 
 /*
 DECLARE @SQL as varchar(max)
 SET @SQL = 'Create or alter View vTABSAMPLE as SELECT TOP 1 * FROM ' + @tbName + ' ORDER BY NEWID()'
 EXEC(@SQL)
 */
-
 META_ONE:
+
 /*
 DECLARE @SQL as varchar(max)
 SET @SQL = 'SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = ' + @tbName + ' ORDER BY 1'
 EXEC(@SQL)
 
 */
-
-
-SELECT 
-	UPPER (
-	Case 
-	     WHEN Data_TYPE IN ( 'varchar', 'char') THEN 'string' + '(' +  CAST (CHARACTER_MAXIMUM_LENGTH as varchar(5)) +')'
-		 WHEN Data_type IN ('time', 'date', 'smalldatetime', 'datetime', 'datetime2', 'datetimeoffset') THEN 'DATETIME'
-		 WHEN data_type IN ('Int', 'bigint', 'smallint', 'tinyint') THEN 'INTEGER' + '(' + CAST (NUMERIC_PRECISION as varchar(5)) + ',' + CAST( NUMERIC_SCALE AS varchar(5)) +')'
-		 WHEN data_type = 'bit' THEN 'BOOLEAN'
-		 ELSE DATA_TYPE + '(' + CAST (NUMERIC_PRECISION as varchar(5)) + ',' + CAST( NUMERIC_SCALE AS varchar(5)) +')'
-	END 
-	) AS	DT_SIZE,
-			@tbName + '.' + COLUMN_NAME as FIELDNAME,
-			IIF(COLUMN_DEFAULT IS NULL, '', COLUMN_DEFAULT) as DEFAULT_VALUE,
-			IIF(IS_NULLABLE = 'NO', ' ', IS_NULLABLE) as IS_NULLABLE
-FROM INFORMATION_SCHEMA.COLUMNS	CC 
-	INNER JOIN INFORMATION_SCHEMA.Tables TT 
-	ON cc.TABLE_NAME = tt.TABLE_NAME and tt.TABLE_NAME = @tbName
-WHERE COLUMN_NAME LIKE '%'+ @colName + '%'
+SELECT UPPER(CASE 
+			WHEN Data_TYPE IN (
+					'varchar',
+					'char'
+					)
+				THEN 'string' + '(' + CAST(CHARACTER_MAXIMUM_LENGTH AS VARCHAR(5)) + ')'
+			WHEN Data_type IN (
+					'time',
+					'date',
+					'smalldatetime',
+					'datetime',
+					'datetime2',
+					'datetimeoffset'
+					)
+				THEN 'DATETIME'
+			WHEN data_type IN (
+					'Int',
+					'bigint',
+					'smallint',
+					'tinyint'
+					)
+				THEN 'INTEGER' + '(' + CAST(NUMERIC_PRECISION AS VARCHAR(5)) + ',' + CAST(NUMERIC_SCALE AS VARCHAR(5)) + ')'
+			WHEN data_type = 'bit'
+				THEN 'BOOLEAN'
+			ELSE DATA_TYPE + '(' + CAST(NUMERIC_PRECISION AS VARCHAR(5)) + ',' + CAST(NUMERIC_SCALE AS VARCHAR(5)) + ')'
+			END) AS DT_SIZE,
+	@tbName + '.' + COLUMN_NAME AS FIELDNAME,
+	IIF(COLUMN_DEFAULT IS NULL, '', COLUMN_DEFAULT) AS DEFAULT_VALUE,
+	IIF(IS_NULLABLE = 'NO', ' ', IS_NULLABLE) AS IS_NULLABLE
+FROM INFORMATION_SCHEMA.COLUMNS CC
+INNER JOIN INFORMATION_SCHEMA.Tables TT
+	ON cc.TABLE_NAME = tt.TABLE_NAME
+		AND tt.TABLE_NAME = @tbName
+WHERE COLUMN_NAME LIKE '%' + @colName + '%'
 ORDER BY 1
 
-
-If left(@tbname,2) = 'tb'
-SELECT 'This is a TB table';
-
+IF left(@tbname, 2) = 'tb'
+	SELECT 'This is a TB table';
 ELSE
-SELECT 'This is a STB table';
+	SELECT 'This is a STB table';
 
 --SELECT * FROM vtabsample  ; --a way to see an example of the actual values. 
-
--- SEARCH FOR AMS TABLE NAME with JOIN.sql
+++ SEARCH FOR AMS TABLE NAME with JOIN.sql
@@ -1,11 +1,13 @@
-use ams
-go
+USE ams
+GO
 
-SELECT tt.TABLE_NAME, cc.COLUMN_NAME
-FROM INFORMATION_SCHEMA.TABLES tt INNER JOIN INFORMATION_SCHEMA.COLUMNS cc
+SELECT tt.TABLE_NAME,
+	cc.COLUMN_NAME
+FROM INFORMATION_SCHEMA.TABLES tt
+INNER JOIN INFORMATION_SCHEMA.COLUMNS cc
 	ON tt.TABLE_NAME = cc.TABLE_NAME
-WHERE tt.table_Name like '%Role%'
+WHERE tt.table_Name LIKE '%Role%'
 --and  left(cc.column_name, 2) = 'vc'
 ORDER BY tt.table_name
+	--SELECT * FROM tbCustName
 
---SELECT * FROM tbCustName
