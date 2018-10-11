c5e43fcd1f877505f0a53f14e513c138d95a7109
Patrick Birch <pbirch999@gmail.com>
2018-10-11 15:47:30 -0500

    Add files via upload

--- ROWCOUNT for ALL TABLES.sql
+++ ROWCOUNT for ALL TABLES.sql
@@ -1,7 +1,18 @@
-DECLARE @TableRowCounts TABLE ([TableName] VARCHAR(128), [RowCount] INT) ;
-INSERT INTO @TableRowCounts ([TableName], [RowCount])
-EXEC sp_MSforeachtable 'SELECT ''?'' [TableName], COUNT(*) [RowCount] FROM ?' ;
-SELECT [TableName], [RowCount]
+DECLARE @TableRowCounts TABLE (
+	[TableName] VARCHAR(128),
+	[RowCount] INT
+	);
+
+INSERT INTO @TableRowCounts (
+	[TableName],
+	[RowCount]
+	)
+EXEC sp_MSforeachtable 'SELECT ''?'' [TableName], COUNT(*) [RowCount] FROM ?';
+
+SELECT [TableName],
+	[RowCount]
 FROM @TableRowCounts
 ORDER BY [TableName]
 GO
+
+
